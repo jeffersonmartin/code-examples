a011ab8ce70d52fc6cb4ccbf260334cd62347440
Jefferson Martin <jeff@jeffersonmartin.com>
2017-08-29 21:25:42 -0700

    Initial commit of laravel/mvc-basics/step-by-step/05

---
+++ laravel/mvc-basics/step-by-step/05_create_a_model_for_event_tickets_table.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Run the following commands to create a model file, which is provides the meta
+# data for each database table and any relationships to other tables.
+
+# Navigate to the top directory of your Laravel application
+cd ~/Sites/presentation/mvc-basics
+
+# Use the the artisan make command to create a new model file.
+php artisan make:model EventTicket
