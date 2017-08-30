8e258079363e4ef26112422018cd8ac227edc4d9
Jefferson Martin <jeff@jeffersonmartin.com>
2017-08-29 21:26:17 -0700

    Initial commit of laravel/mvc-basics/step-by-step/12

---
+++ laravel/mvc-basics/step-by-step/12_create_your_controller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run the following commands to create a controller file, which is a
+# where the magic happens in Laravel for getting data from your database and
+# rendering your view files.
+
+# Navigate to the top directory of your Laravel application
+cd ~/Sites/presentation/mvc-basics
+
+# Use the artisan make command to create a new controller file.
+php artisan make:controller EventTicketController
