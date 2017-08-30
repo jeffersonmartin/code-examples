79057775b8d45e96745012e88e97bcd1a67fadcf
Jefferson Martin <jeff@jeffersonmartin.com>
2017-08-29 21:26:02 -0700

    Initial commit of laravel/mvc-basics/step-by-step/09

---
+++ laravel/mvc-basics/step-by-step/09_add_seed_file_to_seeder.php
@@ -0,0 +1,9 @@
+<?php
+// ~/Sites/presentation/mvc-basics/database/seeds/DatabaseSeeder.php
+// Using your text editor, uncomment the example in the run() method and replace
+// the seed name with `EventTicketDemoSeed`.
+
+public function run()
+{
+    $this->call(EventTicketDemoSeed::class);
+}
