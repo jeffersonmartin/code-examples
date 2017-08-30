890617ef9635881404e8f90023f8893c0f42f719
Jefferson Martin <jeff@jeffersonmartin.com>
2017-08-29 21:25:27 -0700

    Initial commit of laravel/mvc-basics/step-by-step/02

---
+++ laravel/mvc-basics/step-by-step/02_create_a_database_migration.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run the following commands to create a database migration file, which is a
+# abstraction layer recipe for Laravel to perform a CREATE TABLE query.
+
+# Navigate to the top directory of your Laravel application
+cd ~/Sites/presentation/mvc-basics
+
+# Use the Laravel CLI toolset called "artisan" to create a migration file.
+# The "make" command is just a helper. You could make any file by hand.
+php artisan make:migration create_event_tickets_table
