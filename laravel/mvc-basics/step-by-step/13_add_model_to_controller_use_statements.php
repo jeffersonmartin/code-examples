a4b3769f19bc6ef353d63f49c781258b911b1531
Jefferson Martin <jeff@jeffersonmartin.com>
2017-08-29 21:26:23 -0700

    Initial commit of laravel/mvc-basics/step-by-step/13

---
+++ laravel/mvc-basics/step-by-step/13_add_model_to_controller_use_statements.php
@@ -0,0 +1,12 @@
+<?php
+// ~/Sites/presentation/mvc-basics/app/Http/Controllers/EventTicketController.php
+// Using your text editor, add the use statement for the `App\EventTicket` model
+// at the bottom of the existing use statements (above the class).
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\EventTicket;
+
+class EventTicketController extends Controller
+{
