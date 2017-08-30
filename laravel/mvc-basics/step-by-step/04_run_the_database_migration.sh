4489d58b00ea83bcf19cd57b25c0cc704dc6aceb
Jefferson Martin <jeff@jeffersonmartin.com>
2017-08-29 21:25:37 -0700

    Initial commit of laravel/mvc-basics/step-by-step/04

---
+++ laravel/mvc-basics/step-by-step/04_run_the_database_migration.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Run the following commands to run the database migration. The Laravel artisan
+# toolset will automagically determine which migration files have not been run.
+
+# Navigate to the top directory of your Laravel application
+cd ~/Sites/presentation/mvc-basics
+
+# Use artisan to run your database migrations
+php artisan migrate
+
+# You can review the migrations after they've run using the following command
+php artisan migrate:status
+
+# You can now use Sequel Pro (Mac) or your favorite SQL editor to refresh the
+# mvc_basics database and see the newly created `event_tickets` table.
