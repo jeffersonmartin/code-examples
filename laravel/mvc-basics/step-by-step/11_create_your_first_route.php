722223696a25f4108596469c12b4dedad13360a4
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-04 20:15:19 -0700

    Update laravel/mvc-basics/step-by-step/11 to add commas to array

--- laravel/mvc-basics/step-by-step/11_create_your_first_route.php
+++ laravel/mvc-basics/step-by-step/11_create_your_first_route.php
@@ -3,6 +3,6 @@
 // Using your text editor, add the following to the bottom of the file.
 
 Route::get('/event/tickets', [ // If the URL matches this
-    'uses' => 'EventTicketController@index' // Load this controller class name and execute this method (function) name
+    'uses' => 'EventTicketController@index', // Load this controller class name and execute this method (function) name
     'as' => 'event.tickets.index', // We'll also define an internal name for linking to this route
     ]);
