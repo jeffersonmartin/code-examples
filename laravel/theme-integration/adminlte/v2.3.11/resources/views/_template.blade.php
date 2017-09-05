54b165b1dd820af1dde9d3308edc47d1b365922f
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 10:46:28 -0700

    Initial commit of laravel/theme-integration/adminlte/v2.3.11/resources/views/_template

---
+++ laravel/theme-integration/adminlte/v2.3.11/resources/views/_template.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.core')
+
+@section('title')
+    My Page Title
+@endsection
+
+@section('breadcrumbs')
+
+@endsection
+
+@section('content')
+
+    <div class="row">
+
+        <div class="col-sm-12">
+            <div class="box">
+                <div class="box-header">
+                    Title
+                </div>
+                <div class="box-body">
+
+                    Placeholder
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
