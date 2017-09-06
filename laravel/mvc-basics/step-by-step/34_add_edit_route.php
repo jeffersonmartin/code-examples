1ac25a30a71ee1ab0fca3a945eb71aebfa97c545
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 20:27:57 -0700

    Remove laravel/mvc-basics/step-by-step/34 - redundant with routes

--- laravel/mvc-basics/step-by-step/34_add_edit_route.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ~/Sites/presentation/mvc-basics/routes/web.php
-// Using your text editor, add the following to the bottom of the file.
-
-Route::get('/event/tickets/{id}/edit', [
-    'uses' => 'EventTicketController@edit',
-    'as' => 'event.tickets.edit',
-    ]);
