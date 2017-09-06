d32b842e5041956c1a72d7c661e5f83202afc26f
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 20:26:39 -0700

    Update laravel/mvc-basics/step-by-step/14 formatting

--- laravel/mvc-basics/step-by-step/14_create_your_controller_index_method.php
+++ laravel/mvc-basics/step-by-step/14_create_your_controller_index_method.php
@@ -1,28 +1,20 @@
 <?php
 // ~/Sites/presentation/mvc-basics/app/Http/Controllers/EventTicketController.php
-// Using your text editor, add the following method (public function) in the class
-// to replace the `//` comment placeholder.
+// Using your text editor, locate the index() function and replace the `//` placeholder
+// comment with the following code.
 
-use Illuminate\Http\Request;
-use App\EventTicket;
-
-class EventTicketController extends Controller
+public function index()
 {
+    // Get all of the records from the table and return as an object array
+    // that we can loop through in our view file
+    $tickets = EventTicket::get();
 
-    public function index()
-    {
-        // Get all of the records from the table and return as an object array
-        // that we can loop through in our view file
-        $tickets = EventTicket::get();
-
-        // Return page with table of tickets and passing in the tickets variable
-        // that includes the object array of tickets. You don't need to worry
-        // about the theory of the compact method, only to make sure that any
-        // variables that you define above that you want to pass into the view
-        // are included in the compact array (comma separated).
-        return view('event.tickets.index', compact([
-            'tickets'
-            ]));
-    }
-
+    // Return page with table of tickets and passing in the tickets variable
+    // that includes the object array of tickets. You don't need to worry
+    // about the theory of the compact method, only to make sure that any
+    // variables that you define above that you want to pass into the view
+    // are included in the compact array (comma separated).
+    return view('event.tickets.index', compact([
+        'tickets'
+        ]));
 }
