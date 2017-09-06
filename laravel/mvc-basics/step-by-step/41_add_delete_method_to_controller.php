0bdf257597c358561da9343f0028cc11fde4dcb9
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 20:28:50 -0700

    Update laravel/mvc-basics/step-by-step/41 with new destroy method name

--- laravel/mvc-basics/step-by-step/41_add_delete_method_to_controller.php
+++ laravel/mvc-basics/step-by-step/41_add_delete_method_to_controller.php
@@ -1,9 +1,9 @@
 <?php
 // ~/Sites/presentation/mvc-basics/app/Http/Controllers/EventTicketController.php
-// Using your text editor, add the following method (public function) below the
-// existing update() method.
+// Using your text editor, locate the destroy() function and replace the `//` placeholder
+// comment with the following code.
 
-public function delete($id)
+public function destroy($id)
 {
     // Instantiate the EventTicket model based on the $id to update the existing row
     $ticket = EventTicket::findOrFail($id);
