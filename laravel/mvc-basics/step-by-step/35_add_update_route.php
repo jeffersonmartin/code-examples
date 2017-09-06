a7fb5ec96b2877834e41f45546fdef2d5471b604
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 20:28:04 -0700

    Remove laravel/mvc-basics/step-by-step/35 - redundant with routes

--- laravel/mvc-basics/step-by-step/35_add_update_route.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ~/Sites/presentation/mvc-basics/routes/web.php
-// Using your text editor, add the following to the bottom of the file.
-
-Route::post('/event/tickets/{id}', [
-    'uses' => 'EventTicketController@update',
-    'as' => 'event.tickets.update',
-    ]);
