63aa645c5dd9cfef4d5624962ea71c89f4d4c6ef
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 20:25:23 -0700

    Update laravel/mvc-basics/step-by-step/03 with down schema

--- laravel/mvc-basics/step-by-step/03_add_columns_to_migration_file.php
+++ laravel/mvc-basics/step-by-step/03_add_columns_to_migration_file.php
@@ -1,15 +1,20 @@
 <?php
 // ~/Sites/presentation/mvc-basics/database/migrations/YYYY_MM_DD_HHIISS_create_event_tickets_table.php
-// Use your text editor to edit the migration file and add the following code in the `up` method to replace
+// Use your text editor to edit the migration file and add the following code in the methods to replace
 // the placeholder `//` comment. Documentation on other column types is available on the Laravel website.
 //
 
 public function up()
-    {
-        Schema::create('event_tickets', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('ticketholder_name')->nullable();
-            $table->decimal('ticket_price', 10, 2)->default(0.00);
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('event_tickets', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('ticketholder_name')->nullable();
+        $table->decimal('ticket_price', 10, 2)->default(0.00);
+        $table->timestamps();
+    });
+}
+
+public function down()
+{
+    Schema::dropIfExists('event_tickets');
+}
