4e83ab28428f13340981720c42210c829ecdee2b
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 20:28:09 -0700

    Remove laravel/mvc-basics/step-by-step/42 - redundant with routes

--- laravel/mvc-basics/step-by-step/42_add_delete_route.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ~/Sites/presentation/mvc-basics/routes/web.php
-// Using your text editor, add the following to the bottom of the file.
-
-Route::delete('/event/tickets/{id}', [
-    'uses' => 'EventTicketController@delete',
-    'as' => 'event.tickets.delete',
-    ]);
