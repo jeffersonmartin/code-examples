ad01fd8958580f71e303d13d86a7ba3be35712f4
Jefferson Martin <jeff@jeffersonmartin.com>
2017-09-05 20:29:00 -0700

    Update laravel/mvc-basics/step-by-step/43 with theme formatting

--- laravel/mvc-basics/step-by-step/43_add_delete_button_to_index_page.php
+++ laravel/mvc-basics/step-by-step/43_add_delete_button_to_index_page.php
@@ -1,18 +1,22 @@
 <?php
 // ~/Sites/presentation/mvc-basics/resources/views/event/tickets/index.blade.php
 // Using your text editor, edit the `index.blade.php` file and add the <form> section
-// below after the Edit button.
+// and a new delete button.
 
 <tr>
     <td>{{ $ticket->id }}</td>
     <td>{{ $ticket->ticketholder_name }}</td>
     <td>{{ $ticket->ticket_price }}</td>
     <td>
-        <a href="{{ route('event.tickets.edit', ['id' => $ticket->id]) }}">Edit</a>
         <form method="post" action="{{ route('event.tickets.delete', ['id' => $ticket->id]) }}">
             {{ csrf_field() }}
             {!! method_field('delete') !!}
-            <button type="submit" class="btn btn-danger">Delete</button>
+
+            <div class"btn-group">
+                <a class="btn btn-info" href="{{ route('event.tickets.edit', ['id' => $ticket->id]) }}">Edit</a>
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </div>
+
         </form>
     </td>
 </tr>
